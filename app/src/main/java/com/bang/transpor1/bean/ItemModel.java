0460bea5049d74046131b69a0fdcb54ad1d8f010
Bang08 <1245727824@qq.com>
2018-10-13 11:48:26 +0800

    充值的dialogUI

---
+++ app/src/main/java/com/bang/transpor1/bean/ItemModel.java
@@ -0,0 +1,17 @@
+package com.bang.transpor1.bean;
+
+import java.io.Serializable;
+
+public class ItemModel implements Serializable {
+
+    public static final int ONE = 1001;
+    public static final int TWO = 1002;
+
+    public int type;
+    public Object data;
+
+    public ItemModel(int type, Object data) {
+        this.type = type;
+        this.data = data;
+    }
+}
